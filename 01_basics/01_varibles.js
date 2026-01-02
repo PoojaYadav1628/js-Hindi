b27a69a9fb514da9667168f1adde59bd7d626edf
PoojaYadav1628 <pooja16928@gmail.com>
2026-01-02 10:59:21 +0000

    datatpyes

--- 01_basics/01_varibles.js
+++ 01_basics/01_varibles.js
@@ -19,5 +19,22 @@ because of issue in block scope and functional  scope
 */
 
 
+console.log(accountId);// for 1 value 
+console.table ([ accountEmail,accountId,accountPassword,let accountState;
+
+// accountId=2// not allowed to change const
+
+// this way to change value of const 
+accountEmail="hc@hc.com"
+accountPassword ="128734"
+accountCity="Raipur"
+
+/*
+prefer not use var
+because of issue in block scope and functional  scope
+
+*/
+
+
  console.log(accountId);// for 1 value 
 console.table ([ accountEmail,accountId,accountPassword,accountCity,accountState]);   // for multip value change
