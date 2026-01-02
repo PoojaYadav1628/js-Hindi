0e5ae47cdce9440cb3fc90fa05095b08c72a80b8
PoojaYadav1628 <pooja16928@gmail.com>
2026-01-02 10:42:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let accountState;
+
+// accountId=2// not allowed to change const
+
+// this way to change value of const 
+accountEmail="hc@hc.com"
+accountPassword ="128734"
+accountCity="Raipur"
+
+/*
+prefer not use var
+because of issue in block scope and functional  scope
+
+*/
+
+
+ console.log(accountId);// for 1 value 
+console.table ([ accountEmail,accountId,accountPassword,accountCity,accountState]);   // for multip value change
