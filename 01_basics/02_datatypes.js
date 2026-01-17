e881a8c61f184ea709d805dbc4f89b11da72a233
PoojaYadav1628 <pooja16928@gmail.com>
2026-01-17 14:31:17 +0000

    done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,8 +1,26 @@
 "use strict"; // treat all Js code as newer version 
 
-alert(3+3) // we are using node js,not browser
+//alert(3+3) // we are using node js,not browser
 
 console.log (3+
     3); // code reabadility should be high
 
 console.log("Pooja")
+
+let name ="pooja"
+let age =18
+let isLoggedIn = false
+
+// number=>2 to power 53
+// bigint 
+// string =>""
+//boolean => true/false
+//null => standalone value 
+// undefined =>
+    //symbol =>unique 
+
+
+// object 
+
+console.log(typeof null ); // is object    so this output is object 
+console.log (typeof undefined);// is type 
