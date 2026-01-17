0f78b0191f7d9dd581812f781c1861d06825f116
PoojaYadav1628 <pooja16928@gmail.com>
2026-01-17 17:46:44 +0000

    conversion

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -20,5 +20,34 @@ let booleanIsLogggedIn = Boolean(isLoggedIn)
 let someNumber = 33 
 
 let stringNumber =String(someNumber)
-    console.log (stringNumber)
-console.log  (typeof stringNumber);
+ //   console.log (stringNumber)
+//console.log  (typeof stringNumber);
+
+
+//*******************************operation ***************
+
+let value =3 
+let negValue = -value
+//console.log(negValue);
+
+let str1 ="pooja"
+let str2="hello"
+
+let str3 =str1+str2
+//console.log(str3);
+
+//console.log ("1"+2);
+//console.log(1+"2")
+//console.log("1"+2+2);
+//console.log (1+2+"2");
+
+//console.log (+true ) // =>1 //if we write true+ if give error
+//console.log(+"") // then 0 but no sense to make 
+
+let num1, num2, num3
+
+num1 =num2 =num3 =2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
