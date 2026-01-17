931eab597c1221d6edef92b91164f1fb306d8525
PoojaYadav1628 <pooja16928@gmail.com>
2026-01-17 17:09:14 +0000

    coversion datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,24 @@
+let score = "ppoja"
+
+//console.log(typeof  score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+//console.log(typeof  valueInNumber);
+//console.log( valueInNumber);
+// "33"=>33
+//"33abc"=>Nan 
+// true =>1;false=>0
+
+let isLoggedIn = 0
+
+let booleanIsLogggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLogggedIn);
+// 1=>true; 0=>false
+//""=>false 
+//"pooja"=>true 
+let someNumber = 33 
+
+let stringNumber =String(someNumber)
+    console.log (stringNumber)
+console.log  (typeof stringNumber);
